b57f1ea6c2ae1ceefdc8b668dede073832c115af
Skrabka98 <barti.sk@gmail.com>
2020-12-07 17:04:51 +0100

    Initial

---
+++ ML_ObjectDetection/ML_ObjectDetection/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML_ObjectDetection.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
